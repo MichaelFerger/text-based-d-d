7119537d0e75a9ac9c54d03936788058dc0b62b2
pjosephgorman <pjosephgorman@gmail.com>
2020-12-14 13:24:30 -0500

    Update adventure.cpp

--- adventure.cpp
+++ adventure.cpp
@@ -1,5 +1,3 @@
-//this is a comment so I can make sure when I commit, you guys receive this as well
-
 #include <iostream>
 using namespace std;
 
@@ -10,7 +8,7 @@ int main()
                  "What character class are you playng?\n"
                  "[R]ogue\n"
                  "[F]ighter\n"
-                 "[C]leric";
+                 "[C]leric\n";
 
     char charClass;
     std::cin >> charClass;
@@ -31,32 +29,15 @@ int main()
         break;
     }
 
+    int playerHealth = 100;
+    bool hasWeapon = false;
+    char temp;
     char choice1;
     char choice2;
     char choice3;
     char choice4;
 
     std::cout << "You there! Adventurer! Welcome to my dungeon. As you look around, you are sitting in a filthy, dark, damp dungeon.\n"
-<<<<<<< Updated upstream
-                 "On either side of you, right and left, are doors. Your quick glance at the room affords you no other information."
-                 "[O]bserve your surroundings\n" //gives a description of surroundings again
-                 "Go [L]eft!\n"                  //enters the room to the left
-                 "Go [R]ight\n"                  //enters the room to the right
-                 "[G]ive up";                    //ends the game
-
-    std::cin >> choice1;
-
-    switch (toupper(choice1))
-    {
-    case 'O':
-        if (toupper(charClass) == C)
-        {
-            std::cout << "You are in a filthy dungeon, with a door to either side. You also notice a secret door behind you!";
-        }
-        else
-        {
-            std::cout << "You are in a filthy dungeon, with a door to either side.";
-=======
                  "On either side of you, right and left, are doors. Your quick glance at the room affords you no other information.\n"
                  "[O]bserve your surroundings.\n"
                  "Go [L]eft!\n"  //enters the room to the left
@@ -179,11 +160,7 @@ int main()
                         "Go [L]eft.\n"
                         "Go [R]ight.";
             }
->>>>>>> Stashed changes
         }
         break;
     }
-
-    // it is here i realized either i would have to nest all of the switch statements, with each path having every option until
-    // the player either escaped the maze/dungeon or gave up. Looking for another alternative
 }
